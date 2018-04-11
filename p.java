c243183d8a4fcd5ebb82f31eafc11b3646eabc03
goku1713 <prateekparab@gmail.com>
2018-04-11 02:29:29 -0500

    Fixes #94.PSP18SCM54P adding first name

--- p.java
+++ p.java
@@ -8,10 +8,8 @@ Alaphabet p
 " Git Account ID" : PSP18SCM48K
 PSP18SCM99P
 " Git Account ID" : PSP18SCM73A
-" Git Account ID" : PSP18SCM54P
 " Git Account ID" : PSP18SCM73A
 " Git Account-ID" : PSP18SCM48S
-" git Account ID" : PSP18SCM54P
 " Git Account ID" : PSP18SCM73A
 " Git Account ID" : PSP18SCM48K
 " Git Account ID" : PSP18SCM08M
